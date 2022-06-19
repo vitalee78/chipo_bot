f3564a0f904987ced485559a512ca06d5e57e599
Vitaliy Lee <vitalee78@yandex.eu>
2022-06-19 23:37:52 +0700

    added new functions: - do_help - do_time

--- echo/main.py
+++ echo/main.py
@@ -1,9 +1,11 @@
+from datetime import datetime
+
 from telegram import Bot
 from telegram import Update
-from telegram.ext import Updater
 from telegram.ext import CommandHandler
-from telegram.ext import MessageHandler
 from telegram.ext import Filters
+from telegram.ext import MessageHandler
+from telegram.ext import Updater
 
 from echo.config import TG_TOKEN
 
@@ -12,6 +14,15 @@ def do_start(bot: Bot, update: Update):
     bot.send_message(
         chat_id=update.message.chat_id,
         text="Привет! Отправь что-нибудь"
+    )
+
+
+def do_help(bot: Bot, update: Update):
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        text="Это учебный бот\n"
+             "Список доступных команд\n"
+             "Так же я отвечу на любые вопросы"
     )
 
 
@@ -24,14 +35,29 @@ def do_echo(bot: Bot, update: Update):
     )
 
 
+def do_time(bot: Bot, update: Update):
+    date_format = '%d-%m-%Y'
+    today = datetime.now()
+    text = today.strftime(date_format)
+
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        text=text
+    )
+
+
 def main():
     bot = Bot(token=TG_TOKEN)
     updater = Updater(bot=bot)
 
     start_handler = CommandHandler("start", do_start)
+    help_handler = CommandHandler("help", do_help)
+    time_handler = CommandHandler("time", do_time)
     message_handler = MessageHandler(Filters.text, do_echo)
 
     updater.dispatcher.add_handler(start_handler)
+    updater.dispatcher.add_handler(help_handler)
+    updater.dispatcher.add_handler(time_handler)
     updater.dispatcher.add_handler(message_handler)
 
     updater.start_polling()
